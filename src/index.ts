5b6bedc26b8c466a3a8709a09759748bfcc864ab
yashanand167 <yash.anand167@gmail.com>
2026-09-18 03:20:06 +0530

    Basic HTTP Server

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,17 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get("/", (c) => {
+
+  return c.json({
+    message: "Booking Concurrency Lab",
+  });
+
+});
+
+Bun.serve({
+  fetch: app.fetch,
+  port: 3000,
 })
 
 export default app
